71aa0bce1a938442b8f310d8319af44b04aab8d3
Karanjot Singh <karanjotsingh1124@gmail.com>
2018-04-27 21:46:01 +0530

    Animations and project filling

--- js/animate.js
+++ js/animate.js
@@ -10,10 +10,28 @@ function loader() {
         origin: 'right'
     });
 
+    sr.reveal('.card-title', {
+
+        distance: '40px',
+        duration: 2000,
+        origin: "bottom",
+        viewFactor: 1
+    });
+
+    sr.reveal('.card-desc', {
+
+        distance: '40px',
+        duration: 2000,
+        origin: "bottom",
+        viewFactor: 0.5
+    });
+
+    $("header").css('opacity', '1');
 
 };
 
-$(document).ready(function() {
+$(document).ready(function () {
+    $(this).scrollTop(0);
     setTimeout(() => {
         $('nav ul li a').first().addClass('active');
     }, 2000);
@@ -21,43 +39,43 @@ $(document).ready(function() {
 
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-    // $(window).scroll(function() {
-    //     var scroll = $(window).scrollTop();
-    //     var header = $("#header").offset().top - 75;
-    //     var education = $("#education").offset().top - 75;
-    //     var achivement = $("#achivements").offset().top - 75;
-    //     var projects = $("#projects").offset().top - 75;
-    //     var contact = $("#contact_me").offset().top - 75;
-    //     if (scroll < education) {
-    //         $('.active').removeClass();
-    //         $('nav ul li:nth-child(1)').addClass('active');
-    //     } else if (scroll >= education && scroll < achivement) {
-    //         $('.active').removeClass();
-    //         $('nav ul li:nth-child(2)').addClass('active');
-    //     } else if (scroll >= achivement && scroll < projects) {
-    //         $('.active').removeClass();
-    //         $('nav ul li:nth-child(3)').addClass('active');
-    //     } else if (scroll >= projects && scroll < contact) {
-    //         $('.active').removeClass();
-    //         $('nav ul li:nth-child(4)').addClass('active');
-    //     } else if (scroll >= contact) {
-    //         $('.active').removeClass();
-    //         $('nav ul li:nth-child(5)').addClass('active');
-    //     }
-    // });
-    $('nav ul li a').click(function() {
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        var header = $("#header").offset().top - 75;
+        var education = $("#education").offset().top - 75;
+        var achivement = $("#achivements").offset().top - 75;
+        var projects = $("#projects").offset().top - 75;
+        var contact = $("#contact_me").offset().top - 75;
+        if (scroll < education) {
+            $('.active').removeClass();
+            $('nav ul li:nth-child(1) a').addClass('active');
+        } else if (scroll >= education && scroll < achivement) {
+            $('.active').removeClass();
+            $('nav ul li:nth-child(2) a').addClass('active');
+        } else if (scroll >= achivement && scroll < projects) {
+            $('.active').removeClass();
+            $('nav ul li:nth-child(3) a').addClass('active');
+        } else if (scroll >= projects && scroll < contact) {
+            $('.active').removeClass();
+            $('nav ul li:nth-child(4) a').addClass('active');
+        } else if (scroll >= contact) {
+            $('.active').removeClass();
+            $('nav ul li:nth-child(5) a').addClass('active');
+        }
+    });
+    $('nav ul li a').click(function () {
         $('.active').removeClass();
         $(this).addClass('active');
     });
 
-    $('#logo a').click(function() {
+    $('#logo a').click(function () {
         $('.active').removeClass();
         $('nav ul li a').first().addClass('active');
     });
 
-    $('#return-to-top').click(function() {
+    $('#return-to-top').click(function () {
         $('.active').removeClass();
         $('nav ul li a').first().addClass('active');
     });
